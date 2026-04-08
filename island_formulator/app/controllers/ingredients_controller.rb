477af32a6626aaa7fcad6f6b794cb91ffb7ea315
Danielle Deacon <deacondanielle463@gmail.com>
2026-04-08 16:45:53 -0500

    fine tuningsmall issues

--- island_formulator/app/controllers/ingredients_controller.rb
+++ island_formulator/app/controllers/ingredients_controller.rb
@@ -12,7 +12,7 @@ class IngredientsController < ApplicationController
 
   # GET /ingredients/new
   def new
-    @ingredient = Ingredient.new
+    @ingredient = Current.user.Ingredient.new
   end
 
   # GET /ingredients/1/edit
@@ -65,6 +65,6 @@ class IngredientsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def ingredient_params
-  params.require(:ingredient).permit(:name, :category, :description, :photo)
+  params.require(:ingredient).permit(:name, :category, :description, :notes, :photo, tag_ids: [])
     end
 end
