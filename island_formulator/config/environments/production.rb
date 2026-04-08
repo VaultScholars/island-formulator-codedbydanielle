6ea0155e415829e6285fafef1b71c24b09a33202
Danielle Deacon <deacondanielle463@gmail.com>
2026-04-08 12:21:42 -0500

    Update to Configuring Active Storage for DigitalOcean Spaces deployment

--- island_formulator/config/environments/production.rb
+++ island_formulator/config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   # Store uploaded files on DigitalOcean Spaces
-config.active_storage.service = :digital_ocean
+  config.active_storage.service = :digital_ocean
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   # config.assume_ssl = true
